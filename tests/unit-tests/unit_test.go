dafce4057db6d2b2ce052bef29417c87636a75b3
maximusprimeavenger <chuminmaksim@gmail.com>
2025-04-22 23:54:36 +0500

    done tests for callback telegram

--- tests/unit-tests/unit_test.go
+++ tests/unit-tests/unit_test.go
@@ -1,10 +1,16 @@
 package tests_test
 
 import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
 	"telegram-bot/internal/handlers"
 	"telegram-bot/internal/models"
 	"telegram-bot/internal/repository"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -13,19 +19,57 @@ const (
 	fakeToken string = "fake-token-telegram"
 )
 
+/*
+queryChatId := update.CallbackQuery.Message.Chat.Id
+
+	data := update.CallbackQuery.Data
+*/
 func TestHandleCallback(t *testing.T) {
-	update := models.Update{
-		CallbackQuery: &models.CallbackQuery{
-			Data: "help",
-			Message: &models.Message{
-				Chat: &models.Chat{
-					Id: 111,
+	updates := []models.Update{
+		{
+			CallbackQuery: &models.CallbackQuery{
+				Data: "help",
+				Message: &models.Message{
+					Chat: &models.Chat{
+						Id: 111,
+					},
+				},
+			},
+		},
+		{
+			CallbackQuery: &models.CallbackQuery{
+				Data: "test",
+				Message: &models.Message{
+					Chat: &models.Chat{
+						Id: 55,
+					},
+				},
+			},
+		},
+		{
+			CallbackQuery: &models.CallbackQuery{
+				Data: "",
+				Message: &models.Message{
+					Chat: &models.Chat{
+						Id: 5,
+					},
 				},
 			},
 		},
 	}
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	log.SetFlags(0)
+	outputs := make([]string, 3)
 	client := repository.New(fakeHost, fakeToken)
-	handlers.HandleCallback(update, client)
+	for i, update := range updates {
+		buf.Reset()
+		handlers.HandleCallback(update, client)
+		outputs[i] = buf.String()
+	}
+
+	assert.Equal(t, "Unknown callback data: test", strings.TrimSuffix(outputs[1], "\n"), fmt.Sprintf("Not equal outputs:%s", outputs[1]))
+	assert.Equal(t, "Empty callback query data from chat: 5\nUnknown callback data: \n", outputs[2], fmt.Sprintf("Not equal outputs:%s", outputs[2]))
 }
 
 func TestHandleMessage(t *testing.T) {
